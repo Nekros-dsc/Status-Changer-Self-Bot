43a98abd41d8fb7969deb30c73c74acddd15f529
!"Nekros <122643715+Nekros-dsc@users.noreply.github.com>
2023-05-05 19:36:28 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    token: 'Put-Token-Here', // Put your token
+    prefix: 'Prefix', // Put Prefix
+  }
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client } = require('discord.js-selfbot-v13');
+const config = require('./config');
+const prefix = config.client.prefix;
+
+const client = new Client({
+  checkUpdate: false,
+});
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.client.prefix)) return;
+
+  const args = message.content.slice(config.client.prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'status') {
+    let i = 0
+    var status = ["online", "idle", "dnd"]
+    setInterval(() => {
+      if (i === status.length) i = 0
+      client.user.setStatus(status[i]); i++
+    }, 5000);
+    message.edit('Command runs correctly, the state will switch with | `Online`, `Idle`, `Dnd`.')
+  }
+});
+
+client.login(config.client.token);
